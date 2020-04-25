e05e489ccc5cadc3591874ca28adf35a431707f0
p33t <1179301+p33t@users.noreply.github.com>
2020-04-25 11:14:43 -0700

    Add command line argument to select layout

--- src/main/java/tg/App.java
+++ src/main/java/tg/App.java
@@ -11,13 +11,15 @@ import javax.swing.*;
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
+            String config = Util.CONFIG_QWERTY;
+            if (args.length >= 1) config = args[0];
             MainFrame frame = new MainFrame();
             frame.init();
-            frame.setTitle("Typing Game (by freshcode.biz)");
+            frame.setTitle("Typing Game '" + config + "' (by freshcode.biz)");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.pack();
             frame.setVisible(true);
-            frame.start(Util.CONFIG_QWERTY);
+            frame.start(config);
         });
     }
 }
